f10f95505aae81d79526bc46f5e7aabfc9ed6017
kelvin acebron <kacebron@gmail.com>
2025-09-01 17:23:52 +0200

    added defensive column check to debug

--- R/9_LIFT_redox_fit.R
+++ R/9_LIFT_redox_fit.R
@@ -6,62 +6,104 @@
 #' @param df data.table or data.frame from lift_parse_metadata
 #' @param f integer, number of excitation flashlets (e.g. 427)
 #' @param method character vector, one or both of "relative"/"Keller" and "absolute"/"Zendonadi"
+#' @param regression logical; if TRUE, run regression_fit() and return its result
 #'
-#' @return data.table with added columns: Fo, Fm, Fq.Fm, Relative, Fr1, Fr2, Fr
-#' 
-#'  Also can perform Log-Log Regression on LIFT Fluorescence Data
+#' @return data.table with added columns (or the regression result if regression = TRUE)
 #' @export
 redox_fit <- function(df, f, method = c("relative", "absolute"), regression = TRUE) {
 
   # Ensure df is a data.table
   df <- data.table::as.data.table(df)
 
-  # --- Reduction step ---
-  df$Fo <- rep(df[DataPt %in% 1, mean(df$EM), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
-  df$Fm <- rep(df[DataPt %in% 298:302, max(df$EM), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
-  df$Fq.Fm <- (df$Fm - df$Fo) / df$Fm
+  # Defensive column check for clearer errors
+  required <- c("DataPt", "Time.us", "EM", "Target_ID", "TimeSec")
+  missing <- setdiff(required, names(df))
+  if (length(missing)) {
+    stop("`df` is missing required column(s): ", paste(missing, collapse = ", "))
+  }
+
+  # --- Reduction step (use bare column names in data.table expressions) ---
+  df$Fo <- rep(
+    df[DataPt %in% 1, mean(EM), by = .(Target_ID, TimeSec)]$V1,
+    each = f + 1
+  )
+  df$Fm <- rep(
+    df[DataPt %in% 298:302, max(EM), by = .(Target_ID, TimeSec)]$V1,
+    each = f + 1
+  )
+  df$Fq.Fm   <- (df$Fm - df$Fo) / df$Fm
   df$Relative <- (df$EM - df$Fo) / (df$Fm - df$Fo)
 
   # Normalize method names
   method <- tolower(method)
-  method[method %in% "keller"] <- "relative"
+  method[method %in% "keller"]    <- "relative"
   method[method %in% "zendonadi"] <- "absolute"
 
   for (m in method) {
-
     if (m == "relative") {
-      # Keller method
-      df$Fr1.Area <- rep(df[DataPt %in% 303:320, agricolae::audpc(Relative, Time.us, type="absolute"), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
-      df$Fr1.Time <- rep(df[DataPt %in% 320, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1) -
-                      rep(df[DataPt %in% 303, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
+      # Keller method: use Relative
+      df$Fr1.Area <- rep(
+        df[DataPt %in% 303:320, agricolae::audpc(Relative, Time.us, type = "absolute"),
+           by = .(Target_ID, TimeSec)]$V1,
+        each = f + 1
+      )
+      df$Fr1.Time <-
+        rep(df[DataPt %in% 320, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1) -
+        rep(df[DataPt %in% 303, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
       df$Fr1.Fq <- 1 - (df$Fr1.Area / df$Fr1.Time)
 
-      df$Fr2.Area <- rep(df[DataPt %in% 303:360, agricolae::audpc(Relative, Time.us, type="absolute"), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
-      df$Fr2.Time <- rep(df[DataPt %in% 360, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1) -
-                      rep(df[DataPt %in% 303, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
+      df$Fr2.Area <- rep(
+        df[DataPt %in% 303:360, agricolae::audpc(Relative, Time.us, type = "absolute"),
+           by = .(Target_ID, TimeSec)]$V1,
+        each = f + 1
+      )
+      df$Fr2.Time <-
+        rep(df[DataPt %in% 360, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1) -
+        rep(df[DataPt %in% 303, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
       df$Fr2.Fq <- 1 - (df$Fr2.Area / df$Fr2.Time)
 
-      df$Fr.Area <- rep(df[DataPt %in% 303:f, agricolae::audpc(Relative, Time.us, type="absolute"), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
-      df$Fr.Time <- rep(df[DataPt %in% f, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1) -
-                     rep(df[DataPt %in% 303, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
+      df$Fr.Area <- rep(
+        df[DataPt %in% 303:f, agricolae::audpc(Relative, Time.us, type = "absolute"),
+           by = .(Target_ID, TimeSec)]$V1,
+        each = f + 1
+      )
+      df$Fr.Time <-
+        rep(df[DataPt %in% f, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1) -
+        rep(df[DataPt %in% 303, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
       df$Fr.Fq <- 1 - (df$Fr.Area / df$Fr.Time)
 
     } else if (m == "absolute") {
-      # Zendonadi method
-      df$Fr1.Area <- rep(df[DataPt %in% 303:320, agricolae::audpc(EM, Time.us, type="absolute"), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
-      df$Fr1.Time <- rep(df[DataPt %in% 320, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1) -
-                      rep(df[DataPt %in% 303, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
+      # Zendonadi method: use EM and normalize by Fm
+      df$Fr1.Area <- rep(
+        df[DataPt %in% 303:320, agricolae::audpc(EM, Time.us, type = "absolute"),
+           by = .(Target_ID, TimeSec)]$V1,
+        each = f + 1
+      )
+      df$Fr1.Time <-
+        rep(df[DataPt %in% 320, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1) -
+        rep(df[DataPt %in% 303, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
       df$Fr1.Fm <- 1 - (df$Fr1.Area / (df$Fm * df$Fr1.Time))
 
-      df$Fr2.Area <- rep(df[DataPt %in% 303:360, agricolae::audpc(EM, Time.us, type="absolute"), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
-      df$Fr2.Time <- rep(df[DataPt %in% 360, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1) -
-                      rep(df[DataPt %in% 303, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
+      df$Fr2.Area <- rep(
+        df[DataPt %in% 303:360, agricolae::audpc(EM, Time.us, type = "absolute"),
+           by = .(Target_ID, TimeSec)]$V1,
+        each = f + 1
+      )
+      df$Fr2.Time <-
+        rep(df[DataPt %in% 360, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1) -
+        rep(df[DataPt %in% 303, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
       df$Fr2.Fm <- 1 - (df$Fr2.Area / (df$Fm * df$Fr2.Time))
 
-      df$Fr.Area <- rep(df[DataPt %in% 303:f, agricolae::audpc(EM, Time.us, type="absolute"), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
-      df$Fr.Time <- rep(df[DataPt %in% f, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1) -
-                     rep(df[DataPt %in% 303, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
+      df$Fr.Area <- rep(
+        df[DataPt %in% 303:f, agricolae::audpc(EM, Time.us, type = "absolute"),
+           by = .(Target_ID, TimeSec)]$V1,
+        each = f + 1
+      )
+      df$Fr.Time <-
+        rep(df[DataPt %in% f, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1) -
+        rep(df[DataPt %in% 303, mean(Time.us), by = .(Target_ID, TimeSec)]$V1, each = f + 1)
       df$Fr.Fm <- 1 - (df$Fr.Area / (df$Fm * df$Fr.Time))
+
     } else {
       stop("Unknown method: ", m)
     }
@@ -72,18 +114,18 @@ redox_fit <- function(df, f, method = c("relative", "absolute"), regression = TR
   df$Fr1.Time <- NULL
   df$Fr2.Area <- NULL
   df$Fr2.Time <- NULL
-  df$Fr.Area <- NULL
-  df$Fr.Time <- NULL
+  df$Fr.Area  <- NULL
+  df$Fr.Time  <- NULL
   df$Relative <- NULL
 
-  if (regression == TRUE) {
-  data_fit <-  regression_fit(df, f, reorder_cols = TRUE)
-  return(data_fit)
+  if (isTRUE(regression)) {
+    data_fit <- regression_fit(df, f, reorder_cols = TRUE)
+    return(data_fit)
+  } else {
+    redox_fit <- subset(df, DataPt == 303 | DataPt == 321)
+    redox_fit <- redox_fit[!duplicated(redox_fit$TimeSec), ]
+    return(redox_fit)
   }
-  else
-  redox_fit <- subset(df, DataPt == 303 | DataPt == 321)
-  redox_fit <- redox_fit[!duplicated(redox_fit$TimeSec), ]
-  return(redox_fit)
 }
 
 # End
